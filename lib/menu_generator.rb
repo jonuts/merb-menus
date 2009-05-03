c89e2ece0272ef4ec69fd6b245903b0f41c2208b
jonah honeyman <jonah@honeyman.org>
2009-05-03 16:26:56 +0300

    first attemp

--- lib/menu_generator.rb
+++ lib/menu_generator.rb
@@ -1,13 +1,18 @@
 # make sure we're running inside Merb
 if defined?(Merb::Plugins)
-
   # Merb gives you a Merb::Plugins.config hash...feel free to put your stuff in your piece of it
   Merb::Plugins.config[:menu_generator] = {
     :chickens => false
   }
-  
+
   Merb::BootLoader.before_app_loads do
-    # require code that must be loaded before the application
+    Application.class_eval do
+      include MainMenuMixin
+
+      class << self; attr_reader :submenus, :current_menu; end
+
+      @submenus ||= []
+    end
   end
   
   Merb::BootLoader.after_app_loads do
